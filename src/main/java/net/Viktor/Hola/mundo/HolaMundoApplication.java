7f16b3c79a41df551340a96b18ffe02422820de6
Viktor <Viktorjhm.vk@gmail.com>
2021-12-27 19:39:32 -0400

    primer commit

---
+++ src/main/java/net/Viktor/Hola/mundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package net.Viktor.Hola.mundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/Viktor/Hola/mundo/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.Viktor.Hola.mundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
